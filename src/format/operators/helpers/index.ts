66ad2d45934a57bad39ef18f08ff171aff0fd564
Levent Akdogan <lakdogan@2-C-7.42heilbronn.de>
2025-12-25 20:26:21 +0100

    refactor(format): unify spaced binary operator scanning

--- src/format/operators/helpers/index.ts
+++ src/format/operators/helpers/index.ts
@@ -1,8 +1,10 @@
 export { getLeadingOperator } from './get-leading-operator';
 export { findAssignmentRhsStart } from './find-assignment-rhs-start';
-export { findSpacedBinaryOperatorColumn } from './find-spaced-binary-operator-column';
-export { findLastSpacedBinaryOperatorBeforeLimit } from './find-last-spaced-binary-operator-before-limit';
-export { findFirstSpacedBinaryOperatorAfterLimit } from './find-first-spaced-binary-operator-after-limit';
+export {
+  findSpacedBinaryOperatorColumn,
+  findLastSpacedBinaryOperatorBeforeLimit,
+  findFirstSpacedBinaryOperatorAfterLimit
+} from './spaced-binary-operator-scan';
 export { isDashKeywordToken, isSlashDirectiveToken, isSpecialValueToken } from './keyword-tokens';
 export { getPrevToken, isTokenChar, isWhitespace } from './token-utils';
 export { normalizeBinaryOperatorSpacing } from './normalize-binary-operator-spacing';

40b141db5d771b0dae42b30770425cffb864f9ce
Levent Akdogan <lakdogan@2-C-7.42heilbronn.de>
2025-12-26 15:41:15 +0100

    test(rules): add exec sql set current option variants

---
+++ src/scripts/rule-tests/cases/exec-sql-set-current-options.ts
@@ -0,0 +1,21 @@
+import type { Case } from '../types';
+
+const testCase: Case = {
+  name: 'exec-sql-set-current-options',
+  input: [
+    '**free',
+    'exec sql set current isolation = *cs;',
+    'end-exec;',
+    'exec sql set current degree = 2;',
+    'end-exec;',
+    'exec sql set current path = MYLIB, QSYS;',
+    'end-exec;'
+  ].join('\n'),
+  mustInclude: [
+    'set current isolation = *cs;',
+    'set current degree = 2;',
+    'set current path = MYLIB, QSYS;'
+  ]
+};
+
+export default testCase;

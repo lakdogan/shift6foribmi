6df6f8a5c65dbcd78622f3780324c54f13e2c6f0
Levent Akdogan <lakdogan@2-C-7.42heilbronn.de>
2025-12-26 15:30:20 +0100

    test(rules): add exec sql group by rollup/cube case

---
+++ src/scripts/rule-tests/cases/exec-sql-group-by-rollup.ts
@@ -0,0 +1,17 @@
+import type { Case } from '../types';
+
+const testCase: Case = {
+  name: 'exec-sql-group-by-rollup',
+  input: [
+    '**free',
+    'exec sql select ID,sum(AMOUNT) from SALES/ORDERS group by rollup(ID), cube(REGION,TYPE);',
+    'end-exec;'
+  ].join('\n'),
+  mustInclude: [
+    'group by',
+    'rollup(ID),',
+    'cube(REGION, TYPE);'
+  ]
+};
+
+export default testCase;
